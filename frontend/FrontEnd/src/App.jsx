cbc3b574904d729319ca577e9d5d48219a17b48e
Utkarsh3904 <millenniumatom@gmail.com>
2026-05-20 00:38:31 +0530

    refactor: clean up App component and restore CSS flex properties

--- frontend/FrontEnd/src/App.jsx
+++ frontend/FrontEnd/src/App.jsx
@@ -1,49 +1,34 @@
-import axios from 'axios'
-import React from 'react'
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-function App () {
-
-  let [username, setUsername] = useState(null)
-  let [age, setAge] = useState(null)
-  let [city, setCity] = useState(null)
-
-
-  async function getRes() {
-    const res = await fetch("http://localhost:8000/")
-    //need to convert in json
-    // let data = res.json();
-    // data
-    // .then((e)=>{
-    //   console.log(e);
-    // })
-    // .catch((e)=>{
-    //   console.log(e);
-    // })
-
-    axios.post("http://localhost:8000/",{
-      username,  //this is the data we need to send on the server 
-      age,
-      city
-    })   // MORE SIMPLER then fetch
-    .then ((e)=>{
-      console.log(e.data.age);
-    })
-    .catch ((e)=>{
-      console.log(e);
-    })
-  }
-
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      {/* <button onClick = {()=>getRes()}>send</button> */}
-
-      <input type="text" placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)} />
-      <input type="text" placeholder="age" value={age} onChange={(e)=>setAge(e.target.value)} />
-      <input type="text" placeholder="city" value={city} onChange={(e)=>setCity(e.target.value)}  />
-      <button onClick={()=>getRes()}>send</button>
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
